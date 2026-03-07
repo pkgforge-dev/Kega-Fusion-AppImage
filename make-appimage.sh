f509407fa7b8656947b913687dfbf8308faca219
Link4Electronics <link4electronics@gmail.com>
2026-03-07 17:03:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/kega-fusion.png
 export DESKTOP=/usr/share/applications/kega-fusion.desktop
+export STARTUPWMCLASS=Fusion
 export DEPLOY_OPENGL=1
 export LIB_DIR=/usr/lib32
 
