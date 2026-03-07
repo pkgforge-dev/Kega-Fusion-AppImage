d4d82853f578d31a759a6737f919af9c6e1360c2
Link4Electronics <link4electronics@gmail.com>
2026-03-07 10:16:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,13 +12,31 @@ tee -a /etc/pacman.conf <<EOF
 Include = /etc/pacman.d/mirrorlist
 EOF
 pacman -Syu --noconfirm \
-    lib32-alsa-plugins
+    lib32-alsa-plugins  \
+    lib32-atk           \
+    lib32-cairo         \
+    lib32-gdk-pixbuf2   \
+    lib32-glib2         \
+    lib32-libcups       \
+    lib32-librsvg       \
+    lib32-libx11        \
+    lib32-libxcomposite \
+    lib32-libxcursor    \
+    lib32-libxdamage    \
+    lib32-libxext       \
+    lib32-libxfixes     \
+    lib32-libxi         \
+    lib32-libxinerama   \
+    lib32-libxrandr     \
+    lib32-libxrender    \
+    lib32-pango
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package gtk2
 make-aur-package lib32-gtk2
 make-aur-package kega-fusion
 
