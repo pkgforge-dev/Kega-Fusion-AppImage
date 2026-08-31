8b065f4cdc026bb25acc6a0db64efd15def109ae
Link4Electronics <link4electronics@gmail.com>
2026-08-30 21:17:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -33,7 +33,7 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-#get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano gtk2-mini
 
 # Comment this out if you need an AUR package
 make-aur-package lib32-gtk2
